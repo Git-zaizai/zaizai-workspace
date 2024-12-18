4761018f1152a85c70d1d73107a255921d5d8c29
zaizai <2394276262@qq.com>
2024-12-18 23:30:11 +0800

    抽取menu组件

---
+++ packages/piece-zaizai/src/layouts/components/menu.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <n-menu
+    :collapsed-width="64"
+    :collapsed-icon-size="22"
+    :options="menuOptions"
+    v-model:value="selectedKey"
+  />
+</template>
